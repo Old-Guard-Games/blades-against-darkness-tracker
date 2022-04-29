57d0589c18d5ba0e20bfb2785ab15183316b3c32
Craig Burton <craig.adam.burton@gmail.com>
2022-04-29 02:54:28 -0400

    Added +1d indicators to downtime

--- src/features/factions/Faction.component.tsx
+++ src/features/factions/Faction.component.tsx
@@ -4,19 +4,40 @@ import { Gauge } from '../../components/Gauge';
 import { FactionModel, DowntimeActivity } from './Faction.model';
 
 import bandage from '../../img/bandage-roll(1).svg';
+import bandageGold from '../../img/bandage-roll-gold.svg';
+import bandageGray from '../../img/bandage-roll-gray.svg';
 import beer from '../../img/beer-stein(1).svg';
-import hourglass from '../../img/sands-of-time.svg';
+import beerGold from '../../img/beer-stein-gold.svg';
+import beerGray from '../../img/beer-stein-gray.svg';
+import hourglass from '../../img/hourglass.svg';
+import hourglassGold from '../../img/hourglass-gold.svg';
+import hourglassGray from '../../img/hourglass-gray.svg';
 import bag from '../../img/swap-bag(1).svg';
+import bagGold from '../../img/swap-bag-gold.svg';
+import bagGray from '../../img/swap-bag-gray.svg';
 import torch from '../../img/torch(1).svg';
+import torchGold from '../../img/torch-gold.svg';
+import torchGray from '../../img/torch-gray.svg';
 
 const icons = { bandage, beer, hourglass, bag, torch }
+const goldIcons = { bandage: bandageGold, beer: beerGold, hourglass: hourglassGold, bag: bagGold, torch: torchGold };
+const grayIcons = { bandage: bandageGray, beer: beerGray, hourglass: hourglassGray, bag: bagGray, torch: torchGray };
 
 const DowntimeList = ({activities}: {activities: DowntimeActivity[]}) => {
+
   const renderedactivities = activities.map(activity => {
     const {name, icon, description, available, plusOne, minusOne} = activity;
+    const goldStatus = plusOne ? (minusOne ? 'half' : 'full') : 'hide';
+    const divStyle = { width: '1.5em', height: '1.5em'};
     return available ? (
       <div className='downtime-activity'>
-        <h4><img alt={`${icon} icon`} src={icons[icon]} /> {name}</h4>
+        <h4>
+          <div className="stacked-image" style={divStyle}>
+            <img alt={`${icon} icon`} src={icons[icon]} />
+            <img className={`${goldStatus}`} alt={`${icon} gold icon`} src={goldIcons[icon]} />
+          </div>
+          {name}
+        </h4>
         <p className='indented'>{description}</p>
         {plusOne ? (<p className='indented'><strong>+1d:</strong> {plusOne}</p>) : '' }
         {minusOne ? (<p className='indented'><strong>-1d:</strong> {minusOne}</p>) : '' }
@@ -33,7 +54,10 @@ const DowntimeList = ({activities}: {activities: DowntimeActivity[]}) => {
 }
 
 export const Faction = ({faction}: {faction: FactionModel}) => {
-  const {name, tier, description, npcs, assets, allies, enemies, coffers, relationship, downtime, goals} = faction;
+  const {
+    name, tier, description, npcs, assets, allies,
+    enemies, coffers, relationship, downtime, goals
+  } = faction;
 
 
   return (
@@ -103,9 +127,15 @@ export const FactionMenuItem = ({faction}: {faction: FactionModel}) => {
   const rel = faction.relationship > 0 ? '+' + faction.relationship : faction.relationship;
   const iconList = faction.downtime
     .filter((activity) => activity.available)
-    .map((activity, index) => (
-      <img key={index} alt={`${activity.icon} icon`} src={icons[activity.icon]} style={{width: '1.5em'}}/>
-    ));
+    .map((activity, index) => {
+      const goldStatus = activity.plusOne ? (activity.minusOne ? 'half' : 'full') : 'hide';
+      const divStyle = { width: '1.5em', height: '1.5em'};
+      return (
+      <div className="stacked-image" style={divStyle}>
+        <img key={index} alt={`${activity.icon} icon`} src={icons[activity.icon]} />
+        <img key={index} className={`${goldStatus}`} alt={`${activity.icon} gold icon`} src={goldIcons[activity.icon]} />
+      </div>
+    )});
   return (
   <div className='faction-menu'>
     <p className='name'>{faction.name} (T{faction.tier})</p>
